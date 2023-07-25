dbe483a759cdfae0afac0babcb1bb327d25596c8
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-07-25 21:12:44 +0100

    More changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
+import { Roadmap } from "./Components/Roadmap";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       foo: "bar",
-      resumeData: {}
+      resumeData: {},
     };
 
     ReactGA.initialize("UA-110570651-1");
@@ -26,13 +24,13 @@ class App extends Component {
       url: "./resumeData.json",
       dataType: "json",
       cache: false,
-      success: function(data) {
+      success: function (data) {
         this.setState({ resumeData: data });
       }.bind(this),
-      error: function(xhr, status, err) {
+      error: function (xhr, status, err) {
         console.log(err);
         alert(err);
-      }
+      },
     });
   }
 
@@ -44,10 +42,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} />
+        <Roadmap />
         <Footer data={this.state.resumeData.main} />
       </div>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import ParticlesBg from "particles-bg";
 import Fade from "react-reveal";
 
+import logo from "../xxxx.png";
+
 class Header extends Component {
   render() {
     if (!this.props.data) return null;
@@ -13,7 +15,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg color="#FECF08" type="tadpole" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -32,19 +34,19 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#about">
-                About
+                Twitter
               </a>
             </li>
 
             <li>
               <a className="smoothscroll" href="#resume">
-                Resume
+                Telegram
               </a>
             </li>
 
             <li>
               <a className="smoothscroll" href="#portfolio">
-                Works
+                Chart
               </a>
             </li>
 
@@ -59,27 +61,27 @@ class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <Fade bottom>
-              <h1 className="responsive-headline">{name}</h1>
+              <img src={logo} style={{ maxWidth: "500px" }} alt="logo" />
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              <h3>Queensland, strewth mate!</h3>
             </Fade>
             <hr />
-            <Fade bottom duration={2000}>
+            {/* <Fade bottom duration={2000}>
               <ul className="social">
                 <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                  <i className="fa fa-line-chart"></i>Chart
                 </a>
                 <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                  <i className="fa fa-twitter"></i>Twitter
                 </a>
               </ul>
-            </Fade>
+            </Fade> */}
           </div>
         </div>
 
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
+          <a className="smoothscroll" href="#resume">
             <i className="icon-down-circle"></i>
           </a>
         </p>
