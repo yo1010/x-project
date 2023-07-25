7bda67fff23dea551a8293dda820d02bb92e49a7
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-07-25 21:28:19 +0100

    More changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,13 +6,6 @@ import logo from "../xxxx.png";
 
 class Header extends Component {
   render() {
-    if (!this.props.data) return null;
-
-    const project = this.props.data.project;
-    const github = this.props.data.github;
-    const name = this.props.data.name;
-    const description = this.props.data.description;
-
     return (
       <header id="home">
         <ParticlesBg color="#FECF08" type="tadpole" bg={true} />
