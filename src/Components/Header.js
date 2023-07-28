4025fb90352446c009ab22857f17ed8f6a57d3cf
Yavor Dimitrov <ydimitrov@itzamedia.com>
2023-07-28 15:10:38 +0100

    M'

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,13 +9,25 @@ class Header extends Component {
       <header id="home">
         <div className="row banner">
           <div className="banner-text">
-            <Fade bottom>
+            <Fade top>
               <img
                 src={logo}
                 className="xyz-logo"
                 style={{ width: "700px" }}
                 alt="logo"
               />
+            </Fade>
+            <Fade bottom>
+              <h5>
+                Welcome to Project XYZ, a mysterious and enigmatic crypto coin
+                that promises to be unlike any other in the digital currency
+                landscape. Step into a world filled with secrets, puzzles, and
+                intrigue as you embark on a journey to uncover the hidden
+                mysteries behind the XYZ coin. This project aims to challenge
+                the minds of crypto enthusiasts, puzzle solvers, and
+                codebreakers alike, pushing the boundaries of what's possible in
+                the world of blockchain technology.
+              </h5>
             </Fade>
             {/* <Fade bottom duration={2000}>
               <ul className="social">
